c028c895b845a16b51e6b54e42f3420eabd2284c
Adeepta Shushil <adeepto04@gmail.com>
2024-05-11 12:48:24 +0600

    added code in task1 and task4

--- src/Assignment1/Task1/Program.cs
+++ src/Assignment1/Task1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int size = int.Parse(Console.ReadLine());
+
+int[,] array = new int[size, size];
+
+for (int i = 0; i < size; i++)
+{
+    string values = Console.ReadLine();
+
+    string[] stringArray = values.Split(' ');
+
+    for (int j = 0; j < stringArray.Length; j++)
+    {
+        array[i, j] = int.Parse(stringArray[j]);
+    }
+}
+
+int firstDiagonalSum = 0;
+int secondDiagonalSum = 0;
+
+for (int i = 0; i < size; i++)
+{
+    firstDiagonalSum += array[i, i];
+    secondDiagonalSum += array[i, size - 1 - i];
+}
+
+Console.WriteLine($" 1st Diagonal Sum: {firstDiagonalSum}");
+Console.WriteLine($" 2nd Diagonal Sum: {secondDiagonalSum}");
--- src/Assignment1/Task4/Program.cs
+++ src/Assignment1/Task4/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int number;
+
+while (true)
+{
+    number = int.Parse(Console.ReadLine());
+
+    if (number == 0)
+    {
+        break;
+    }
+
+    int isPrime = 0;
+
+    for (int i = 1; i <= number; i++)
+    {
+        if (number % i == 0)
+        {
+            isPrime++;
+        }
+    }
+
+    if (isPrime == 2)
+    {
+        Console.WriteLine("Yes");
+    }
+    else
+    {
+        Console.WriteLine("No");
+    }
+}
+
