64a2da253a8cc5a92256af2e248a8ce9466c6752
Adeepta Shushil <adeepto04@gmail.com>
2024-05-11 17:14:28 +0600

    task2 completed.

--- src/Assignment1/Task2/Program.cs
+++ src/Assignment1/Task2/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string input = Console.ReadLine();
+
+string[] numbers = input.Split(',');
+
+for(int i = 0;  i < numbers.Length; i++)
+{
+    numbers[i] = numbers[i].Trim();
+}
+
+int[] values = new int[numbers.Length];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    values[i] = int.Parse(numbers[i]);
+}
+
+for (int i = 0; i < numbers.Length - 1; i++)
+{
+    for (int j = 0; j < numbers.Length - i - 1; j++)
+    {
+        if (values[j] < values[j + 1])
+        {
+            int temp = values[j];
+            values[j] = values[j + 1];
+            values[j + 1] = temp;
+        }
+    }
+}
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write(values[i]);
+
+    if (i < numbers.Length - 1)
+    {
+        Console.Write(", ");
+    }
+}
