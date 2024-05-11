82396208175d900f08172c263dba5f22039e6467
Adeepta Shushil <adeepto04@gmail.com>
2024-05-11 18:05:21 +0600

    task3 completed

--- src/Assignment1/Task3/Program.cs
+++ src/Assignment1/Task3/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string numLine = Console.ReadLine();
+
+string[] numbers = numLine.Split(',');
+
+for(int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = numbers[i].Trim();
+}
+
+double[] nums = new double[numbers.Length];
+
+for(int i = 0; i< numbers.Length;i++)
+{
+    nums[i] = double.Parse(numbers[i]);
+}
+
+double largest = 0;
+double secondLargest = 0;
+
+foreach (double number in nums)
+{
+    if (number > largest)
+    {
+        secondLargest = largest;
+        largest = number;
+    }
+}
+
+Console.WriteLine(secondLargest);
