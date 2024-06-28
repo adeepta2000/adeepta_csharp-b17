7c64e3eaeb2e8524e4724d27794f27069577483a
Adeepta Shushil <adeepto04@gmail.com>
2024-06-28 19:51:17 +0600

    Task1 Completed

--- src/Assignment2/Task1/Product.cs
+++ src/Assignment2/Task1/Product.cs
@@ -12,5 +12,23 @@ namespace Task1
         public double Price { get; set; }
         public string Description { get; set; }
         public string Color { get; set; }
+
+        public Product() : this("Product Name", 0.0)
+        {
+
+        }
+
+        public Product(string name, double price) : this(name, price, "Product Description", "Product Color")
+        {
+
+        }
+
+        public Product(string name, double price, string description, string color)
+        {
+            Name = name;
+            Price = price;
+            Description = description;
+            Color = color;
+        }
     }
 }
--- src/Assignment2/Task1/Program.cs
+++ src/Assignment2/Task1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Task1;
+
+Product product1 = new Product();
+
+Product product2 = new Product("Pen", 10.00);
+
+Product product3 = new Product("4B Pencil", 20.00, "Latest 4B Pencil", "Black");
+
+Console.WriteLine(product1.Name);
+Console.WriteLine(product1.Price);
+Console.WriteLine(product1.Description);
+Console.WriteLine(product1.Color);
+
+Console.WriteLine("--------------------");
+
+Console.WriteLine(product2.Name);
+Console.WriteLine(product2.Price);
+Console.WriteLine(product2.Description);
+Console.WriteLine(product2.Color);
+
+Console.WriteLine("--------------------");
+
+Console.WriteLine(product3.Name);
+Console.WriteLine(product3.Price);
+Console.WriteLine(product3.Description);
+Console.WriteLine(product3.Color);
