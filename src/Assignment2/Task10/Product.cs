2d8d8adaeb7623fc12e93747afd8b0c08db664df
Adeepta Shushil <adeepto04@gmail.com>
2024-06-29 11:01:47 +0600

    Task10 Completed

--- src/Assignment2/Task10/Product.cs
+++ src/Assignment2/Task10/Product.cs
@@ -11,5 +11,10 @@ namespace Task10
         public string Name { get; set; }
         public string Description { get; set; }
         public string Image { get; set; }
+
+        ~Product()
+        {
+
+        }
     }
 }
