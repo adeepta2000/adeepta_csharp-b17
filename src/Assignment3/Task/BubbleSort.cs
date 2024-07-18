7928625344d8224657dc47439e9b6ba2ad7262df
Adeepta Shushil <adeepto04@gmail.com>
2024-07-18 23:08:18 +0600

    Uploading Assignment3.

---
+++ src/Assignment3/Task/BubbleSort.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    public class BubbleSort<T> where T : IComparable<T>
+    {
+        public void SortArray(T[] array) 
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j].CompareTo(array[j + 1]) > 0)
+                    {
+                        T temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/Assignment3/Task/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using Task;
+
+BubbleSort<int> bubbleSort1 = new BubbleSort<int>();
+BubbleSort<double> bubbleSort2 = new BubbleSort<double>();
+
+int[] intArray = { 8, 3, 7, 6, 9 };
+double[] doubleArray = { 8.65, 3.78, 7.12, 6.92, 9.38 };
+
+bubbleSort1.SortArray(intArray);
+bubbleSort2.SortArray(doubleArray);
+
+Console.WriteLine(string.Join(", ", intArray));
+Console.WriteLine(string.Join(", ", doubleArray));
